8d6ea2d3b76fbacfb7459f4111c2508bd41e7ddf
Alverto Ortega <ortegagarcia678@gmail.com>
2022-05-24 01:58:45 -0400

    queried local data via site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,11 @@
 //define site's metadata, plugins and other general configs:
 module.exports = {
+    siteMetadata:{
+        siteUrl: 'https://ortegaportfolio.com',
+        name: 'Alverto Ortega',
+        role: 'Freelance Developer',
+        bio: 'Passionate at learning the Full-stack Web development technology'
+    },
     plugins:[
         'gatsby-plugin-postcss'
     ],
