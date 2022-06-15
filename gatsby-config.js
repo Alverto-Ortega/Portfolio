2364b0f016fd1951f26660192f90c0bb2e82b7b4
Alverto Ortega <ortegagarcia678@gmail.com>
2022-06-15 14:04:58 -0400

    Created blog posts template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 //define site's metadata, plugins and other general configs:
+require("dotenv").config()
 module.exports = {
     siteMetadata:{
         siteUrl: 'https://ortegaportfolio.com',
@@ -11,10 +12,24 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `mdx-bio`,
-                path: `${__dirname}/MDX`,
+                path: `${__dirname}/MDX/bio`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `mdx-blog`,
+                path: `${__dirname}/MDX/blog`,
             },
         },
         `gatsby-plugin-mdx`,
         'gatsby-plugin-postcss',
+        'gatsby-plugin-image',
+        {
+            resolve: 'gatsby-source-graphcms',
+            options: {
+                endpoint: process.env.GRAPHCMS_ENDPOINT,
+            },
+        },
     ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,53 @@
 //this file can be used to create pages programmatically based on dynamic data...
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+//createPages function to create pages dynamically:
+exports.createPages = async ({actions, graphql, reporter }) => {
+    //destructure actions object to retrive createPage function
+    const { createPage } = actions;
+    //tell gatsby where to find my blog post template:
+    const BlogPostTemplate = path.resolve('./src/templates/blog-page.js');
+    //query data using GraphQL:
+    const BlogPostQuery = await graphql(`
+        {
+            allMdx(filter: {frontmatter: { type: { eq: "Blog" }}}){
+                nodes {
+                    fields{
+                        slug
+                    }
+                }
+            }
+        }
+    `);
+    //catch errors if unsuccessful:
+    if(BlogPostQuery.errors){
+        reporter.panicOnBuild("Error while running GraphQL query.");
+        return;
+    }
+    //upon success, data is gathered. Now we can create pages for each data node
+    BlogPostQuery.data.allMdx.nodes.forEach(({
+        fields: { slug } }) => {
+            createPage({
+                path: `blog${slug}`,
+                component: BlogPostTemplate,
+                // to build pages the first time it is requested instead of waiting for all to be built at build time
+                defer: true,
+                context: {
+                    slug: slug,
+                },
+            });
+        });
+
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions;
+    if( node.internal.type === `Mdx`){
+        const slug = createFilePath({ node, getNode, basePath: `pages` });
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        });
+    }
+};
